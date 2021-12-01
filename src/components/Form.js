7d886914a64ecf1ccfb209d100561688d0730c45
codingSlate <ananta2021k@gmail.com>
2021-12-01 19:10:05 +0530

    count error fixed

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,46 +2,78 @@ import React from 'react';
 import './Form.css';
 import { useState, useEffect, useReducer } from 'react';
 
-const formReducerFn = (latestState, action) => {
-  if (action.type === 'NAME') {
-    return { ...latestState, userName: action.value };
-  }
-  if (action.type === 'AMOUNT') {
-    return { ...latestState, userAmount: action.value };
-  }
+// -------------------------------------------- reducer function that is called when action trigger
+// const formReducerFn = (latestState, action) => {
+//   if (action.type === 'NAME') {
+//     return { ...latestState, userName: action.value };
+//   }
+//   if (action.type === 'AMOUNT') {
+//     return { ...latestState, userAmount: action.value };
+//   }
 
-  // default value
-  return { userName: 'Name', userAmount: 'Amount' };
-};
-
+//   // default value
+//   return { userName: 'Name', userAmount: 'Amount' };
+// };
+// --------------------------------------------
 function Form() {
-  const [formReducer, setFormReducer] = useReducer(formReducerFn, {
-    userName: 'Enter Name',
-    userAmount: 'Enter Amount',
-  });
+  // -------------------------------------------- useReducer
+  // const [formReducer, setFormReducer] = useReducer(formReducerFn, {
+  //   userName: 'Enter Name',
+  //   userAmount: 'Enter Amount',
+  // });
 
+  // -------------------------------------------- useState
   const [form, setForm] = useState({
     userName: 'any name',
     userAmount: 'any amount',
   });
 
+  // count character
+  const [countChar, setCountChar] = useState('');
+
+  // const [isValid, setIsValid] = useState(false);
+  // -------------------------------------------- useEffect
+  useEffect(() => {
+    let millisec = 500;
+    console.log('once');
+
+    const clearTimerID = setTimeout(() => {
+      if (form.userName.length > 0) {
+        console.log(`I am with cleanup 😃 ${millisec} milisecond`);
+        setCountChar((c) => (c = form.userName.length));
+      } else {
+        console.log(`should not be empty`);
+      }
+    }, millisec);
+
+    return () => {
+      clearTimeout(clearTimerID);
+    };
+  }, [form.userName, form.userAmount]);
+
+  // --------------------------------------------
   const nameChangeHandler = (e) => {
-    setFormReducer({ type: 'NAME', value: e.target.value });
     setForm((prevState) => {
       return { ...prevState, userName: e.target.value };
     });
+    // setFormReducer({ type: 'NAME', value: e.target.value });
   };
+
+  // --------------------------------------------
   const amountChangeHandler = (e) => {
-    setFormReducer({ type: 'AMOUNT', value: e.target.value });
     setForm((prevState) => {
       return { ...prevState, userAmount: e.target.value };
     });
+    // setFormReducer({ type: 'AMOUNT', value: e.target.value });
+    console.log('ssssssssss', countChar);
   };
 
+  // --------------------------------------------
   const formSubmitHandler = (e) => {
     e.preventDefault();
-   setForm({userName:'', userAmount:''}) // reset form after submit
+    setForm({ userName: '', userAmount: '' }); // reset form after submit
   };
+  // --------------------------------------------
 
   return (
     <>
@@ -50,18 +82,24 @@ function Form() {
         <input
           className="input"
           type="text"
-          value={form.userName}
+          // value={form.userName}
           onChange={nameChangeHandler}
         />
+        {/* {!isValid && (
+          <span className="input-error">Name should not be empty</span>
+        )} */}
         <input
           className="input"
           type="text"
-          value={form.userAmount}
+          // value={form.userAmount}
           onChange={amountChangeHandler}
         />
-
+        {/* {!isValid && (
+          <span className="input-error">Amount should not be empty</span>
+        )} */}
         <input className="input" type="submit" />
       </form>
+      <p>Total characters: {countChar}</p>
     </>
   );
 }
