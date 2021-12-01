c5ad0b2e6bab1b574dbbaaac434c54219a264b99
codingSlate <ananta2021k@gmail.com>
2021-12-01 17:39:22 +0530

    1 file modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,30 +1,51 @@
 import React from 'react';
 import './Form.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
+const formReducerFn = (latestState, action) => {
+  if (action.type === 'NAME') {
+    return { ...latestState, userName: action.value };
+  }
+  if (action.type === 'AMOUNT') {
+    return { ...latestState, userAmount: action.value };
+  }
+
+  // default value
+  return { userName: 'Name', userAmount: 'Amount' };
+};
 function Form() {
+  const [formReducer, setFormReducer] = useReducer(formReducerFn, {
+    userName: 'Enter Name',
+    userAmount: 'Enter Amount',
+  });
 
-const nameChangeHandler =(e)=>{
-  
-}
-const amountChangeHandler =(e)=>{
-  
-}
+  const nameChangeHandler = (e) => {
+    setFormReducer({ type: 'NAME', value: e.target.value });
+  };
+  const amountChangeHandler = (e) => {
+    setFormReducer({ type: 'AMOUNT', value: e.target.value });
+  };
 
-const formSubmitHandler =()=>{
-  e.preventDefault()
-}
+  const formSubmitHandler = () => {
+    e.preventDefault();
+  };
 
   return (
     <>
       <h2>Form</h2>
       <form onSubmit={formSubmitHandler}>
-        <input className="input" type="text" 
-        value={}
-        onChange={nameChangeHandler}/>
-        <input className="input" type="text" 
-        value={}
-        onChange={amountChangeHandler}/>
+        <input
+          className="input"
+          type="text"
+          value={formReducer.userName}
+          onChange={nameChangeHandler}
+        />
+        <input
+          className="input"
+          type="text"
+          value={formReducer.userAmount}
+          onChange={amountChangeHandler}
+        />
 
         <input className="input" type="submit" />
       </form>
